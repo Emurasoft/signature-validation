d76e438d6c60896039b92f99d5ba31d506ef3e70
Makoto Emura <makoto@emurasoft.com>
2026-06-11 22:05:52 -0700

    ValidatePESignature

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"crypto/x509"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
-	"os/exec"
 	"time"
 
 	"github.com/pkg/errors"
@@ -106,166 +104,9 @@ func downloadToTemp(url string) (string, error) {
 	return tmpFile.Name(), nil
 }
 
-// SignatureInfo represents the output of Get-AuthenticodeSignature.
-type SignatureInfo struct {
-	SignerCertificate SignerCertificate
-	Status            int
-	StatusMessage     string
-	Path              string
-}
-
-// SignerCertificate represents the signer certificate information.
-type SignerCertificate struct {
-	NotAfter  time.Time
-	NotBefore time.Time
-	RawData   []byte
-	Subject   SubjectInfo
-}
-
-func (sc *SignerCertificate) UnmarshalJSON(b []byte) error {
-	type Alias SignerCertificate
-	aux := &struct {
-		*Alias
-	}{
-		Alias: (*Alias)(sc),
-	}
-	if err := json.Unmarshal(b, &aux); err != nil {
-		return errors.WithStack(err)
-	}
-
-	if len(sc.RawData) > 0 {
-		subject, err := ExtractSubjectInfo(sc.RawData)
-		if err != nil {
-			return errors.Errorf("failed to extract subject info: %w", err)
-		}
-		sc.Subject = subject
-	}
-
-	return nil
-}
-
-// SubjectInfo represents the parsed Subject field of a certificate.
-type SubjectInfo struct {
-	CommonName         string
-	Organization       string
-	OrganizationalUnit string
-	Locality           string
-	State              string
-	Country            string
-}
-
-func (s *SubjectInfo) UnmarshalJSON(b []byte) error {
-	// We don't need to unmarshal from string anymore if we have RawData,
-	// but the JSON still contains the Subject string. We can ignore it or
-	// keep it if we want to support both.
-	// For now, let's just do nothing here and let RawData handle it.
-	return nil
-}
-
-// ExtractSubjectInfo extracts SubjectInfo from raw certificate bytes.
-func ExtractSubjectInfo(rawData []byte) (SubjectInfo, error) {
-	cert, err := x509.ParseCertificate(rawData)
-	if err != nil {
-		return SubjectInfo{}, errors.WithStack(err)
-	}
-
-	info := SubjectInfo{
-		CommonName: cert.Subject.CommonName,
-	}
-	if len(cert.Subject.Organization) > 0 {
-		info.Organization = cert.Subject.Organization[0]
-	}
-	if len(cert.Subject.OrganizationalUnit) > 0 {
-		info.OrganizationalUnit = cert.Subject.OrganizationalUnit[0]
-	}
-	if len(cert.Subject.Locality) > 0 {
-		info.Locality = cert.Subject.Locality[0]
-	}
-	if len(cert.Subject.Province) > 0 {
-		info.State = cert.Subject.Province[0]
-	}
-	if len(cert.Subject.Country) > 0 {
-		info.Country = cert.Subject.Country[0]
-	}
-
-	return info, nil
-}
-
-// getSignatureInfo calls Get-AuthenticodeSignature and returns the parsed info.
-func getSignatureInfo(filePath string) (SignatureInfo, error) {
-	cmdStr := fmt.Sprintf(
-		"Get-AuthenticodeSignature '%s' | Select-Object @{Name='SignerCertificate'; Expression={$_.SignerCertificate | Select-Object NotAfter, NotBefore, Subject, RawData}}, Status, StatusMessage | ConvertTo-Json",
-		filePath,
-	)
-
-	cmd := exec.Command("pwsh",
-		"-NoProfile",
-		"-NonInteractive",
-		"-Command", cmdStr,
-	)
-
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		return SignatureInfo{}, errors.Errorf(
-			"powershell command failed: %w; stdout/stderr:\n%s",
-			err, string(out),
-		)
-	}
-
-	var info SignatureInfo
-	if err := json.Unmarshal(out, &info); err != nil {
-		return SignatureInfo{}, errors.Errorf("failed to parse json: %w; raw:\n%s", err, string(out))
-	}
-
-	return info, nil
-}
-
 type ValidationResult struct {
 	Valid  bool   `json:"valid"`
 	Reason string `json:"reason,omitempty"`
-}
-
-// validateSignature validates the signature information.
-func validateSignature(info SignatureInfo) ValidationResult {
-	if info.Status != 0 {
-		return ValidationResult{
-			Valid:  false,
-			Reason: fmt.Sprintf("invalid signature status: %s (status code: %d)", info.StatusMessage, info.Status),
-		}
-	}
-
-	subject := info.SignerCertificate.Subject
-	if subject.CommonName != "Emurasoft, Inc." {
-		return ValidationResult{
-			Valid:  false,
-			Reason: fmt.Sprintf("unexpected Common Name: %s", subject.CommonName),
-		}
-	}
-
-	if subject.Organization != "Emurasoft, Inc." {
-		return ValidationResult{
-			Valid:  false,
-			Reason: fmt.Sprintf("unexpected Organization: %s", subject.Organization),
-		}
-	}
-
-	if subject.State != "Washington" {
-		return ValidationResult{
-			Valid:  false,
-			Reason: fmt.Sprintf("unexpected State: %s", subject.State),
-		}
-	}
-
-	if subject.Country != "US" {
-		return ValidationResult{
-			Valid:  false,
-			Reason: fmt.Sprintf("unexpected Country: %s", subject.Country),
-		}
-	}
-
-	return ValidationResult{
-		Valid: true,
-	}
 }
 
 func mainWithError() (*ValidationResult, error) {
@@ -285,12 +126,21 @@ func mainWithError() (*ValidationResult, error) {
 
 	fmt.Fprintf(os.Stderr, "File downloaded to: %s\n", path)
 
-	info, err := getSignatureInfo(path)
+	f, err := os.Open(path)
 	if err != nil {
-		return nil, err
+		return nil, errors.WithStack(err)
 	}
+	defer f.Close()
 
-	result := validateSignature(info)
+	var result ValidationResult
+	if err := ValidatePESignature(f); err != nil {
+		result = ValidationResult{
+			Valid:  false,
+			Reason: err.Error(),
+		}
+	} else {
+		result = ValidationResult{Valid: true}
+	}
 
 	// Clean up
 	if err := os.Remove(path); err != nil {
@@ -311,7 +161,8 @@ func main() {
 		Time: time.Now().UTC(),
 	}
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "error: %+v\n", output.Error)
+		output.Error = err.Error()
+		fmt.Fprintf(os.Stderr, "error: %+v\n", err)
 	} else {
 		output.Result = result
 	}
